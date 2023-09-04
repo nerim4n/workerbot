f8161064c053029412257b3ac29cb64209e12ce5
Nəriman Məmmədli <50991261+nerim4n@users.noreply.github.com>
2023-09-04 17:07:20 +0400

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from dateutil import parser
-from datetime import datetime, timedelta
-
+import datetime
 URL = "https://az.wikipedia.org/w/api.php"
+
+def chop_microseconds(delta):
+    return delta - datetime.timedelta(microseconds=delta.microsecond)
 
 def get_data_from_api(params):
     try:
@@ -14,13 +16,30 @@ def get_data_from_api(params):
         return None
 
 def is_template_outdated(page):
-    # Existing code for checking if the template is outdated
-    # ...
+    params = {
+        "action": "query",
+        "prop": "revisions",
+        "titles": page['title'],
+        "rvslots": "main",
+        "formatversion": "2",
+        "format": "json"
+    }
+    data = get_data_from_api(params)
 
-def remove_specific_text_from_page(page_content):
-    # Modify this function to remove specific text from the page content
-    # For example, replace "TextToRemove" with an empty string to remove it
-    return page_content.replace("TextToRemove", "")
+    if not data:
+        return False
+
+    pages = data.get("query", {}).get("pages", [])
+    if pages:
+        revision = pages[0].get("revisions", [])[0]
+        if revision:
+            insertion_date = parser.parse(revision.get("timestamp", ""))
+            right_now_7_days_ago = datetime.datetime.today() - datetime.timedelta(days=7)
+
+            if insertion_date.replace(tzinfo=None) < chop_microseconds(right_now_7_days_ago):
+                return True
+
+    return False
 
 def main():
     params = {
@@ -39,43 +58,6 @@ def main():
     for page in pages:
         if is_template_outdated(page):
             print(u'\u2713', page['title'], "şablon silinməlidir")
-
-            # Fetch the current page content
-            content_params = {
-                "action": "query",
-                "prop": "revisions",
-                "titles": page['title'],
-                "rvslots": "main",
-                "formatversion": "2",
-                "format": "json"
-            }
-            content_data = get_data_from_api(content_params)
-            
-            if content_data:
-                pages = content_data.get("query", {}).get("pages", [])
-                if pages:
-                    revision = pages[0].get("revisions", [])[0]
-                    if revision:
-                        page_content = revision.get("slots", {}).get("main", {}).get("content", "")
-
-                        # Remove specific text if condition is true
-                        modified_content = remove_specific_text_from_page(page_content)
-                        
-                        # Update the page content
-                        edit_params = {
-                            "action": "edit",
-                            "title": page['title'],
-                            "token": "YOUR_EDIT_TOKEN",  # Obtain the edit token (similar to previous examples)
-                            "text": modified_content,
-                            "summary": "Edit via API",
-                            "format": "json"
-                        }
-                        edit_response = get_data_from_api(edit_params)
-                        
-                        if edit_response and edit_response.get("edit", {}).get("result") == "Success":
-                            print(f"Removed specific text from '{page['title']}'")
-                        else:
-                            print(f"Failed to remove specific text from '{page['title']}'")
         else:
             print(u'\u2717', page['title'], "şablon silinməməlidir")
 
